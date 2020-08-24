9e561d9ebdc5e32bb4bb56b2d61cc06c5bc9c3c6
asep10001 <a2h2hemingway@gmail.com>
2020-08-24 22:09:29 +0700

    tes 1

---
+++ tugas-7/project-01/src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import NavBAr from './component/navbar'
+
+
+class App extends Component {
+  render(){
+    return (
+      <>
+        <NavBAr/>
+        <h1>Hello World</h1>
+        <h1>Hello World</h1>
+      </>
+    )
+  }
+}
+
+export default App;
