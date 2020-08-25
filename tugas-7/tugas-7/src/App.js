d9c37a0ea5cc0795918570c7d66a8a6af3371728
asep10001 <a2h2hemingway@gmail.com>
2020-08-25 09:13:38 +0700

    adding ui kit

--- tugas-7/tugas-7/src/App.js
+++ tugas-7/tugas-7/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import '../../node_modules/uikit/dist/css/uikit.min.css'
+import '../../node_modules/uikit/dist/js/uikit.min.js'
+import '../../node_modules/uikit/dist/js/uikit-icons.min.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  render(){
+    return (
+      <>
+      <h1>HELLO</h1>
+      </>
+    )
+  }
 }
 
 export default App;
