2710286330cb5e6581b0039dae0c6c2d33c85783
asep10001 <a2h2hemingway@gmail.com>
2020-09-03 09:08:41 +0700

    memperbaiki tampilan card

--- tugas-11/tugas-11/src/pages/input/index.js
+++ tugas-11/tugas-11/src/pages/input/index.js
@@ -60,23 +60,33 @@ export class InputData extends Component {
       },
       {
         img:
-          "https://lh3.googleusercontent.com/OUGpBp4h8BirMb1zHrmFXyEtIxEqkLaFD4FkyeL7qSY5aypicd-0XihmUlK7XH8eU-YU4NSGYUaYbavScaht=w2640-h1986-rw",
-        name: "Asep Agus Heri Hermawan",
-        moto:
-          "he who didn't taste the bitterness of learning, will suffer the humiliation of ignorance for the rest of his life.",
-        github: "https://github.com/asep10001",
+          "https://ch3302files.storage.live.com/y4mHeyy0uY1Y-A-Aq1jJu7TW5jJSYlyI1c3yts37BlKu1ACjnkAg-sO8jKx9BFL9ZSeslJoJR9xzukZ2aYcuQmWi55x3EBZJpDr_KfiHoBNdLVOCtuBnLmlBJpfnBFr6n0rxqYRla9h7wpTzR1KNiCju727xWdv8TAk_vErUJ5bq3RxGwxgXRmSXB_jo-DfkvX39lCulb90zy0HRPgbf4cKfQ/pas%20foto%20%28merah%3Bno%20glasses%29.jpg?psid=1&width=390&height=585",
+        name: "Pramadhio Ari Putro",
+        moto: "Khawatir adalah penyalahgunaan sebuah imajinasi",
+        github: "https://github.com/dhioputro",
       },
       {
         img:
-          "https://lh3.googleusercontent.com/OUGpBp4h8BirMb1zHrmFXyEtIxEqkLaFD4FkyeL7qSY5aypicd-0XihmUlK7XH8eU-YU4NSGYUaYbavScaht=w2640-h1986-rw",
-        name: "Asep Agus Heri Hermawan",
-        moto:
-          "he who didn't taste the bitterness of learning, will suffer the humiliation of ignorance for the rest of his life.",
-        github: "https://github.com/asep10001",
+          "https://lh6.googleusercontent.com/lYSI9LX18lDAZDk1aBL80jXhy6XKbbsw3S6zimjjVMiYMcIT6jqJaYrngHfvgOV-nP4wW8pr_wipWzIDHwL9=w2640-h1986-rw",
+        name: "Rifki Harbi Awali",
+        moto: "Basthotan Fil Ilmi Wal Jismi",
+        github: "https://github.com/rifkiharbiawali",
+      },
+      {
+        img:
+          "https://media-exp1.licdn.com/dms/image/C5103AQHSe1OxoQuwog/profile-displayphoto-shrink_200_200/0?e=1604534400&v=beta&t=eicpGCTB4aWrT1wHBRmNgZVbyOEeI8NYZ6AbqR9qN8Y",
+        name: "Yoseph Mario Wibowo",
+        moto: "Ora Et Labora",
+        github: "https://github.com/YosephMarioWibowo/",
       },
     ];
   }
 
+  componentDidMount(){
+    this.setState({
+      studentsData: this.dataArray
+    })
+  }
   //untuk mendapatkan user data kita:
   //onchangeValue
 
